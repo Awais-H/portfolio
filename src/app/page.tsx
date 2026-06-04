42d0427e73b642249dc2b650a8849ea9953edb22
Awais Hashar <awais.hashar@gmail.com>
2026-06-04 01:59:35 -0400

    updated icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
                 <AvatarImage
                   alt={DATA.name}
                   src={DATA.avatarUrl}
-                  className="object-cover object-[center_45%]"
+                  className="object-cover object-[center_90%]"
                 />
                 <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
